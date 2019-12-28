393c2e2b9aa6a45b586f98276b1cfe0802ba768f
Josh Mahowald <joshua.mahowald@gmail.com>
2019-12-28 11:52:03 -0600

    guard brnaches

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/bash -ex
 
+
+
+
+export BRANCH_NAME=${GITHUB_REF##*/}
+
+if [ "$BRANCH_NAME" != "$INPUT_ONLY_IF_BRANCH" ]; then
+    echo "current branch is not $INPUT_ONLY_IF_BRANCH.  Ignoring "
+    exit 0
+fi
+
+only_if_branch
+
+
 export GITHUB_TOKEN=$INPUT_GITHUB_TOKEN
 export GITHUB_USER=${GITHUB_USER:-$INPUT_GITHUB_USER}
 export INPUT_REMOTE_REPO_BRANCH=${INPUT_REMOTE_REPO_BRANCH:-develop}
 repo_dir=$(echo $INPUT_REMOTE_REPO_NAME  | cut -d "/" -f 2)
 
-env
+
+
+
 if [ ! -d $repo_dir ]; then
     git clone --depth 1 \
     https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$INPUT_REMOTE_REPO_NAME.git \
